d07fd5ce59fba8a6f8a7c5a51f82b7c47d0a7829
Dasha Mishkina <dasha.mishkina117@gmail.com>
2020-06-05 02:27:45 +0300

    Notifications

--- app/src/main/java/com/example/tomatoclock/MainActivity.kt
+++ app/src/main/java/com/example/tomatoclock/MainActivity.kt
@@ -1,5 +1,10 @@
 package com.example.tomatoclock
 
+import android.app.Notification
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.content.Intent
 import android.media.AudioManager
 import android.media.SoundPool
 import androidx.appcompat.app.AppCompatActivity
@@ -17,6 +22,37 @@ class MainActivity : AppCompatActivity() {
     var restTime: Int = 5
     var isWork: Boolean = true
     val soundPool = SoundPool(6, AudioManager.STREAM_MUSIC, 0)
+    val id1 = "What for is this id?"
+    val id2 = "Probably, I understood this"  // Then... But what for name?
+
+    fun notifications(isWork: Boolean) {
+        val notificationIntent = Intent(this, MainActivity::class.java)
+        val contentIntent = PendingIntent.getActivity(
+            this,
+            0,
+            notificationIntent,
+            PendingIntent.FLAG_CANCEL_CURRENT
+        )
+        val notificationChannel1: NotificationChannel = NotificationChannel(id1, "channel1", NotificationManager.IMPORTANCE_DEFAULT)
+        val notificationManager = getSystemService(NOTIFICATION_SERVICE) as NotificationManager
+        val builder1 = Notification.Builder(this, id1)
+        val notificationChannel2: NotificationChannel = NotificationChannel(id2, "channel2", NotificationManager.IMPORTANCE_DEFAULT)
+        val builder2 = Notification.Builder(this, id1)
+        notificationManager.createNotificationChannel(notificationChannel1)
+        builder1.setContentIntent(contentIntent)
+            .setSmallIcon(R.drawable.ic_launcher_foreground)
+            .setContentTitle("TomatoClock")
+            .setContentText("It's time to work!")
+        notificationManager.createNotificationChannel(notificationChannel2)
+        builder2.setContentIntent(contentIntent)
+            .setSmallIcon(R.drawable.ic_launcher_foreground)
+            .setContentTitle("TomatoClock")
+            .setContentText("It's time to rest!")
+        if (isWork)
+            notificationManager.notify(100, builder1.build())
+        else
+            notificationManager.notify(100, builder2.build())
+    }
 
     fun renewTime() {
         time_View.text = (time_counter / 60).toString() + ":" + (time_counter % 60).toString()
@@ -24,6 +60,7 @@ class MainActivity : AppCompatActivity() {
             if (time_counter / 60 >= workTime) {
                 isWork = false
                 soundPool.play(1, 1F, 1F, 0, 0, 1F)
+                notifications(isWork)
                 state_View.text = "Rest"
                 time_counter = 0
             }
@@ -31,6 +68,7 @@ class MainActivity : AppCompatActivity() {
             if (time_counter / 60 >= restTime) {
                 isWork = true
                 soundPool.play(1, 1F, 1F, 0, 0, 1F)
+                notifications(isWork)
                 state_View.text = "Work"
                 time_counter = 0
             }
@@ -53,7 +91,6 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
         soundPool.load(baseContext, R.raw.ding, 1)
         reset_btn.setOnClickListener {
             timer.cancel()
